2d04a05788ee13ffd15347f9a009229945576555
bash <115079501+bashified@users.noreply.github.com>
2026-08-27 07:07:58 +0530

    fixed Vendor mismatch

--- src/misc.py
+++ src/misc.py
@@ -45,11 +45,11 @@ def resolve_hostname(ip: str) -> str:
 def vendor_from_mac(mac: str) -> str:
     prefix = mac.upper().replace(":", "").replace("-", "")[:6]
     oui = {
-        "DCFB48": "Apple",    "F0D1A9": "Apple",   "A8BB50": "Apple",
-        "88A29E": "Apple",    "F0DCE2": "Apple",    "BC9FEF": "Apple",
+        "DCFB48": "Intel",    "F0D1A9": "Apple",   "A8BB50": "WiZ IoT",
+        "88A29E": "Raspberry Pi",    "F0DCE2": "Apple",    "BC9FEF": "Apple",
         "E0D4E8": "Intel",    "B827EB": "Raspberry Pi", "DCA632": "Raspberry Pi",
         "001A11": "Google",   "94EB2C": "Google",
-        "00E04C": "Realtek",  "4CCC6A": "Intel",
+        "00E04C": "Realtek",  "4CCC6A": "MSI",
         "00155D": "Microsoft (Hyper-V)", "000C29": "VMware",
         "080027": "VirtualBox",
         "18D6C7": "Amazon Echo", "FC65DE": "Amazon",
